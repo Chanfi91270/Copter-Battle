2ba649911e868e0290c22078033382eb0eb49a87
yanis-91 <20230719@etud.univ-evry.fr>
2026-03-13 16:09:25 +0100

    new features

--- classes/obstacle.py
+++ classes/obstacle.py
@@ -3,17 +3,25 @@ import pygame
 SPEED = 5
 
 class Obstacle:
-    def __init__(self, x: int, y: int, is_transparent: bool, lives: int):
+    def __init__(self, x: int, y: int, image : pygame.Surface, is_transparent: bool, lives: int, mobile: bool, armed: bool):
         self.x = x
         self.y = y
+        self.image = image
         self.is_transparent = is_transparent
         self.lives = lives
+        self.mobile = mobile
+        self.armed = armed
 
     def move(self):
-        while self.lives > 0:
-            self.x -= SPEED
-            self.y -= SPEED
-            self.y += SPEED
+        if self.mobile:
+            while self.lives > 0:
+                self.x -= SPEED
+                self.y -= SPEED
+                self.y += SPEED
+
+    def shoot(self):
+        if self.armed:
+            pass
 
     def take_damage(self):       
         self.lives -= 1
