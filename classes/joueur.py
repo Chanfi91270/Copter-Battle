353f0548998089b987e309cbcced0bf1957329d7
yanis-91 <20230719@etud.univ-evry.fr>
2026-03-22 23:32:41 +0100

    update sockets

--- classes/joueur.py
+++ classes/joueur.py
@@ -1,11 +1,17 @@
 import pygame
 import socket
 
+PORT = 5555
+
 class Joueur:
     def __init__(self, nom: str, score: int, server : bool):
         self.nom = nom
         self.score = score
         self.server = server
+
+        self.conn = None
+        self.ecouteur = None
+        self.connected = False
 
     def augmenter_score(self, points: int):
         self.score += points
@@ -16,30 +22,26 @@ class Joueur:
     def est_server(self):
         return self.server
 
-    def set_join(self):
+    def set_join(self,ip: str=""):
         if self.est_server():
-            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server.bind(("0.0.0.0", 5555))  # écoute sur le port 5555
-            server.listen(1)
-
+            self.ecouteur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.ecouteur.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.ecouteur.bind(("0.0.0.0", PORT))  # écoute sur le port 5555
+            self.ecouteur.listen(1)
+            self.ecouteur.setblocking(False)
             print("En attente d'un joueur...")
-            conn, addr = server.accept()
-            print("Connecté à", addr)
 
-            while True:
-                data = conn.recv(1024).decode()
-                if not data:
-                    break
-                print("Reçu:", data)
-                conn.send("OK".encode())
         else:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.conn.setblocking(False)
+            try:
+                self.conn.connect((ip, PORT))
+            except BlockingIOError:
+                pass
+              
 
-            client.connect(("IP_DU_SERVEUR", 5555))
-
-            while True:
-                msg = input("Message: ")
-                client.send(msg.encode())
-
-                response = client.recv(1024).decode()
-                print("Serveur:", response)     
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+        if self.ecouteur:
+            self.ecouteur.close()  
