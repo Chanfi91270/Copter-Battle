077c9c99b2eee51185f586cda280e9184aca0f9f
yanis-91 <20230719@etud.univ-evry.fr>
2026-03-23 13:35:01 +0100

    Add collision detection to Helicopter class

--- classes/helico.py
+++ classes/helico.py
@@ -4,19 +4,17 @@ SPEED = 5
 
 class Helicopter:
     def __init__(self, x: int, y: int, has_shield: bool, is_transparent: bool, lives: int):
-        self.x = x
-        self.y = y
         self.has_shield = has_shield
         self.is_transparent = is_transparent
         self.lives = lives
-        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        self.rect = self.image.get_rect(topleft=(x, y))
 
     def move(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]: self.x -= SPEED
-        if keys[pygame.K_RIGHT]: self.x += SPEED
-        if keys[pygame.K_UP]: self.y -= SPEED
-        if keys[pygame.K_DOWN]: self.y += SPEED
+        if keys[pygame.K_LEFT]: self.rect.x -= SPEED
+        if keys[pygame.K_RIGHT]: self.rect.x += SPEED
+        if keys[pygame.K_UP]: self.rect.y -= SPEED
+        if keys[pygame.K_DOWN]: self.rect.y += SPEED
 
     def toggle_shield(self):
         self.has_shield = not self.has_shield
